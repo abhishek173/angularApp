29200cc508be6666c04abee3091bf25ff9610a2d
abhishek173 <abhishekkumar801104@gmail.com>
2024-03-21 07:48:21 +0530

    Added top-navbar

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MyContainerComponent } from './my-container/my-container.component';
+import { TopnavComponent } from './my-container/topnav/topnav.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyContainerComponent,
+    TopnavComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
